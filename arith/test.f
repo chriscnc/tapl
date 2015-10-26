f31196bc0c5053baababa854c8e4aca5fe30335b
Chris <chris.cornelison@gmail.com>
2015-10-26 08:33:39 -0500

    extend tests

--- arith/test.f
+++ arith/test.f
@@ -1,8 +1,11 @@
 /* Examples for testing */
 
 true;
+false;
 if false then true else false; 
-
+if true then true else false;
+if true then 0 else (succ 0);
+if false then 0 else (succ 0);
 0; 
 succ (pred 0);
 iszero (pred (succ (succ 0))); 
